a774cfea6b9d7728d7b8ffd4014bd893e9da6e62
Nevena <nevena.rogova@gmx.at>
2024-10-01 08:08:33 +0200

    nested_loops_exercise
    SoftUni

---
+++ nested_loops_exercise/test.py
@@ -0,0 +1,9 @@
+text = "Gosho"
+print(text[0])
+print(text[1])
+# print(text[10])
+print(len(text))
+
+text = "Gosho"
+for index, character in enumerate(text):
+    print(f"Index = {index},Value ={character}")
